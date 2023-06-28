456fa1189be7112295a2aa8c839aa20e745d62c3
Luiz Henrique <luizhjramos@outlook.com>
2023-06-28 08:31:23 -0300

    feat: user entity struct

---
+++ internal/users/entity.go
@@ -0,0 +1,14 @@
+package users
+
+import "time"
+
+type User struct {
+	ID         int64     `json:"id"`
+	Name       string    `json:"name"`
+	Login      string    `json:"login"`
+	Password   string    `json:"password"`
+	CreatedAt  time.Time `json:"created_at"`
+	ModifiedAt time.Time `json:"modified_at"`
+	Deleted    bool      `json:"deleted"`
+	LastLogin  time.Time `json:"last_login"`
+}

3397794b55ab2ad379dd876ed5a082add36b8c19
Luiz Henrique <luizhjramos@outlook.com>
2023-06-28 08:41:24 -0300

    feat: create user with encrypted password

--- internal/users/entity.go
+++ internal/users/entity.go
@@ -1,6 +1,11 @@
 package users
 
-import "time"
+import (
+	"crypto/md5"
+	"errors"
+	"fmt"
+	"time"
+)
 
 type User struct {
 	ID         int64     `json:"id"`
@@ -11,4 +16,36 @@ type User struct {
 	ModifiedAt time.Time `json:"modified_at"`
 	Deleted    bool      `json:"deleted"`
 	LastLogin  time.Time `json:"last_login"`
+}
+
+func New(name, login, password string) (*User, error) {
+	now := time.Now()
+
+	user := &User{
+		Name:       name,
+		Login:      login,
+		CreatedAt:  now,
+		ModifiedAt: now,
+	}
+
+	err := user.SetPassword(password)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (u *User) SetPassword(password string) error {
+	if password == "" {
+		return errors.New("password is required and can't be empty")
+	}
+
+	if len(password) < 6 {
+		return errors.New("password must be at least 6 characters long")
+	}
+
+	u.Password = fmt.Sprintf("%x", (md5.Sum([]byte(password))))
+
+	return nil
 }

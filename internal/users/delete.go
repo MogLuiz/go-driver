37cdbace6847dfa1c3f5c0f818525e4dba6006dc
Luiz Henrique <luizhjramos@outlook.com>
2023-06-28 23:21:03 -0300

    feat: add delete user handler

---
+++ internal/users/delete.go
@@ -0,0 +1,39 @@
+package users
+
+import (
+	"database/sql"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/go-chi/chi"
+)
+
+func (h *handler) Delete(w http.ResponseWriter, r *http.Request) {
+	urlId := chi.URLParam(r, "id")
+	if urlId == "" {
+		http.Error(w, ErrIdRequired.Error(), http.StatusBadRequest)
+		return
+	}
+
+	id, err := strconv.Atoi(urlId)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = Delete(h.db, int64(id))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Add("content-type", "application/json")
+}
+
+func Delete(db *sql.DB, id int64) error {
+	stmt := `UPDATE "users" set "modified_at"=$1, "deleted"=true  where "id"=$2`
+	_, err := db.Exec(stmt, time.Now(), id)
+
+	return err
+}

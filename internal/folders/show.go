264b4d0a47fc76871b22d2f978fce360c4987de7
Luiz Henrique <luizhjramos@outlook.com>
2023-06-30 08:06:29 -0300

    feat: add folder content eager loading

--- internal/folders/show.go
+++ internal/folders/show.go
@@ -22,18 +22,30 @@ func (h handler) ShowById(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	folder, err := SelectById(h.db, int64(id))
+	folder, err := SelectFolderById(h.db, int64(id))
 	if err != nil {
 		// TODO: validade if error is because user not found
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
+	content, err := SelectFolderContent(h.db, int64(id))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	folderContent := FolderContent{
+		Folder:  *folder,
+		Content: content,
+	}
+
 	w.Header().Add("content-type", "application/json")
-	json.NewEncoder(w).Encode(folder)
+	json.NewEncoder(w).Encode(folderContent)
+
 }
 
-func SelectById(db *sql.DB, id int64) (*Folder, error) {
+func SelectFolderById(db *sql.DB, id int64) (*Folder, error) {
 	stmt := `select * from "folders" where "id"=$1`
 	row := db.QueryRow(stmt, id)
 
@@ -44,4 +56,46 @@ func SelectById(db *sql.DB, id int64) (*Folder, error) {
 	}
 
 	return &f, nil
+}
+
+func selectSubFolders(db *sql.DB, folderID int64) ([]Folder, error) {
+	stmt := `select * from "folders" where "parent_id"=$1 and "deleted"=false`
+	rows, err := db.Query(stmt, folderID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	folders := make([]Folder, 0)
+	for rows.Next() {
+		var folder Folder
+		err := rows.Scan(&folder.ID, &folder.ParentID, &folder.Name, &folder.CreatedAt, &folder.ModifiedAt, &folder.Deleted)
+		if err != nil {
+			continue
+		}
+
+		folders = append(folders, folder)
+	}
+
+	return folders, nil
+}
+
+func SelectFolderContent(db *sql.DB, folderID int64) ([]FolderResource, error) {
+	subFolders, err := selectSubFolders(db, folderID)
+	if err != nil {
+		return nil, err
+	}
+
+	folderResource := make([]FolderResource, 0, len(subFolders))
+	for _, subFolder := range subFolders {
+		folderResource = append(folderResource, FolderResource{
+			ID:         subFolder.ID,
+			Name:       subFolder.Name,
+			Type:       "directory",
+			CreatedAt:  subFolder.CreatedAt,
+			ModifiedAt: subFolder.ModifiedAt,
+		})
+	}
+
+	return folderResource, nil
 }

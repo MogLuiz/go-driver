7d82df2a3602ab8ced4c37ed73586a61acf51f22
Luiz Henrique <luizhjramos@outlook.com>
2023-07-24 06:47:33 -0300

    test: ensure list root files database method works

--- internal/files/list_test.go
+++ internal/files/list_test.go
@@ -31,3 +31,27 @@ func TestList(t *testing.T) {
 		t.Error(err)
 	}
 }
+
+func TestListRoot(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Error(err)
+	}
+	defer db.Close()
+
+	rows := sqlmock.NewRows([]string{"id", "folder_id", "owner_id", "name", "type", "path", "created_at", "modified_at", "deleted"}).
+		AddRow(1, nil, 1, "Gopher.png", "test", "test", time.Now(), time.Now(), false).
+		AddRow(2, nil, 1, "Golang-LOGO.png", "test", "test", time.Now(), time.Now(), false)
+
+	mock.ExpectQuery(regexp.QuoteMeta(`select * from "files" where "folder_id" is null and "deleted"=false`)).
+		WillReturnRows(rows)
+
+	_, err = ListRoot(db)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Error(err)
+	}
+}

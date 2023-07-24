14e68d12c013366db0bc035528065acba0ad48be
Luiz Henrique <luizhjramos@outlook.com>
2023-07-24 06:52:51 -0300

    refactor: abstract logic to select all files to a utility method

--- internal/files/list.go
+++ internal/files/list.go
@@ -4,28 +4,15 @@ import "database/sql"
 
 func List(db *sql.DB, folderID int64) ([]File, error) {
 	stmt := `select * from "files" where "folder_id"=$1 and "deleted"=false`
-	rows, err := db.Query(stmt, folderID)
-	if err != nil {
-		return nil, err
-	}
-
-	files := make([]File, 0)
-	for rows.Next() {
-		var file File
-
-		err := rows.Scan(&file.ID, &file.FolderID, &file.OwnerID, &file.Name, &file.Type, &file.Path, &file.CreatedAt, &file.ModifiedAt, &file.Deleted)
-		if err != nil {
-			continue
-		}
-
-		files = append(files, file)
-	}
-
-	return files, nil
+	return selectAllFiles(db, stmt)
 }
 
 func ListRoot(db *sql.DB) ([]File, error) {
 	stmt := `select * from "files" where "folder_id" is null and "deleted"=false`
+	return selectAllFiles(db, stmt)
+}
+
+func selectAllFiles(db *sql.DB, stmt string) ([]File, error) {
 	rows, err := db.Query(stmt)
 	if err != nil {
 		return nil, err

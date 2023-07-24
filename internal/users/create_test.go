ea01a992834e4dcad5f7e9623bb8820a4eef0320
Luiz Henrique <luizhjramos@outlook.com>
2023-07-24 14:26:27 -0300

    test: ensure be able create user with http request

--- internal/users/create_test.go
+++ internal/users/create_test.go
@@ -1,10 +1,56 @@
 package users
 
 import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"regexp"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
 )
+
+func TestCreate(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Error(err)
+	}
+	defer db.Close()
+
+	h := handler{db: db}
+
+	u := User{
+		Name:     "John Doe",
+		Login:    "john.doe@golang.com",
+		Password: "12345",
+	}
+
+	var b bytes.Buffer
+	err = json.NewEncoder(&b).Encode(&u)
+	if err != nil {
+		t.Error(err)
+	}
+
+	recordResponse := httptest.NewRecorder()
+	request := httptest.NewRequest(http.MethodPost, "/", &b)
+
+	mock.ExpectExec(regexp.QuoteMeta(`insert into "users" ("name", "login", "password", "modified_at") values ($1, $2, $3, $4)`)).
+		WithArgs(u.Name, u.Login, u.Password, u.ModifiedAt).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	h.Create(recordResponse, request)
+
+	result := recordResponse.Body.String()
+
+	if recordResponse.Code != http.StatusCreated {
+		t.Errorf("Expected status code 201, got %v", result)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Error(err)
+	}
+}
 
 func TestInsert(t *testing.T) {
 	db, mock, err := sqlmock.New()
@@ -18,7 +64,7 @@ func TestInsert(t *testing.T) {
 		t.Error(err)
 	}
 
-	mock.ExpectExec(`insert into "users" ("name", "login", "password", "modified_at")*`).
+	mock.ExpectExec(regexp.QuoteMeta(`insert into "users" ("name", "login", "password", "modified_at") values ($1, $2, $3, $4)`)).
 		WithArgs(user.Name, user.Login, user.Password, user.ModifiedAt).
 		WillReturnResult(sqlmock.NewResult(1, 1))
 

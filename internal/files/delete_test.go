edd0b1366096d6df1b52a0e1ef0cdf8a7f295319
Luiz Henrique <luizhjramos@outlook.com>
2023-08-17 08:00:45 -0300

    test(e2e): ensure be able to delete file

--- internal/files/delete_test.go
+++ internal/files/delete_test.go
@@ -43,3 +43,24 @@ func TestDeleteHTTP(t *testing.T) {
 		t.Error(err)
 	}
 }
+
+func TestDelete(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Error(err)
+	}
+	defer db.Close()
+
+	mock.ExpectExec(regexp.QuoteMeta(`update "files" set "modified_at"=$1, "deleted"=true  where "id"=$2`)).
+		WithArgs(utils.AnyTime{}, 1).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	err = Delete(db, 1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Error(err)
+	}
+}
